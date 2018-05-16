6580c64cdc65d52dfcacc9ebfcd34be7fc2359da
Johann Petrak <johann.petrak@gmail.com>
2018-05-16 23:09:43 +0100

    Forgot to commit the tests.

---
+++ src/test/java/com/jpetrak/gate/jdbclookup/tests/GappLoadingTest.java
@@ -0,0 +1,16 @@
+package com.jpetrak.gate.jdbclookup.tests;
+
+import gate.test.GappLoadingTestCase;
+
+/**
+ * Using this class automatically tests all pipelines for proper loading.
+ * 
+ * This class automatically tries to load all pipelines (any file with an 
+ * extension ".gapp" or ".xgapp") which reside
+ * in the main/resources/resources directory tree.
+ * 
+ * NOTE: please move/copy this class to the correct package for your plugin!
+ */
+public class GappLoadingTest extends GappLoadingTestCase {
+  
+}
